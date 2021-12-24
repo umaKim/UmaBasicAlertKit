df2e77fff144d1b6e3884f748f31fc5239d9acf8
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2021-12-25 02:34:00 +0900

    fix: deleted some lines

--- Sources/UmaBasicAlertKit/UmaBasicAlertKit.swift
+++ Sources/UmaBasicAlertKit/UmaBasicAlertKit.swift
@@ -35,41 +35,41 @@ public extension UIViewController {
     }
     
     // MARK: 커스텀 하단 경고창
-    func presentBottomAlert(message: String, target: ConstraintRelatableTarget? = nil, offset: Double? = -12) {
-        let alertSuperview = UIView()
-        alertSuperview.backgroundColor = UIColor.black.withAlphaComponent(0.9)
-        alertSuperview.layer.cornerRadius = 10
-        alertSuperview.isHidden = true
-    
-        let alertLabel = UILabel()
-        alertLabel.font = .NotoSans(.regular, size: 15)
-        alertLabel.textColor = .white
-        
-        self.view.addSubview(alertSuperview)
-        alertSuperview.snp.makeConstraints { make in
-            make.bottom.equalTo(target ?? self.view.safeAreaLayoutGuide).offset(-12)
-            make.centerX.equalToSuperview()
-        }
-        
-        alertSuperview.addSubview(alertLabel)
-        alertLabel.snp.makeConstraints { make in
-            make.top.equalTo(6)
-            make.bottom.equalTo(-6)
-            make.leading.equalTo(12)
-            make.trailing.equalTo(-12)
-        }
-        
-        alertLabel.text = message
-        alertSuperview.alpha = 1.0
-        alertSuperview.isHidden = false
-        UIView.animate(
-            withDuration: 2.0,
-            delay: 1.0,
-            options: .curveEaseIn,
-            animations: { alertSuperview.alpha = 0 },
-            completion: { _ in
-                alertSuperview.removeFromSuperview()
-            }
-        )
-    }
+//    func presentBottomAlert(message: String, target: ConstraintRelatableTarget? = nil, offset: Double? = -12) {
+//        let alertSuperview = UIView()
+//        alertSuperview.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+//        alertSuperview.layer.cornerRadius = 10
+//        alertSuperview.isHidden = true
+//
+//        let alertLabel = UILabel()
+//        alertLabel.font = .NotoSans(.regular, size: 15)
+//        alertLabel.textColor = .white
+//
+//        self.view.addSubview(alertSuperview)
+//        alertSuperview.snp.makeConstraints { make in
+//            make.bottom.equalTo(target ?? self.view.safeAreaLayoutGuide).offset(-12)
+//            make.centerX.equalToSuperview()
+//        }
+//
+//        alertSuperview.addSubview(alertLabel)
+//        alertLabel.snp.makeConstraints { make in
+//            make.top.equalTo(6)
+//            make.bottom.equalTo(-6)
+//            make.leading.equalTo(12)
+//            make.trailing.equalTo(-12)
+//        }
+//
+//        alertLabel.text = message
+//        alertSuperview.alpha = 1.0
+//        alertSuperview.isHidden = false
+//        UIView.animate(
+//            withDuration: 2.0,
+//            delay: 1.0,
+//            options: .curveEaseIn,
+//            animations: { alertSuperview.alpha = 0 },
+//            completion: { _ in
+//                alertSuperview.removeFromSuperview()
+//            }
+//        )
+//    }
 }
