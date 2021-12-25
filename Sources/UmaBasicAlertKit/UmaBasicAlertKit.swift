96cb071be8862a6b44411d5685bdeb99b9a37a69
hrKim0303 <85341050+hrKim0303@users.noreply.github.com>
2021-12-25 18:48:28 +0900

    changed paramemters

--- Sources/UmaBasicAlertKit/UmaBasicAlertKit.swift
+++ Sources/UmaBasicAlertKit/UmaBasicAlertKit.swift
@@ -5,13 +5,13 @@ public extension UIViewController {
     func presentAlert(title: String,
                       message: String? = nil,
                       isCancelActionIncluded: Bool = false,
-//                      actionDoneButtonTitle: String? = "확인",
+                      actionDoneButtonTitle: String = "확인",
                       actionCancelButtonTitle: String = "취소",
                       preferredStyle style: UIAlertController.Style = .alert,
                       handler: ((UIAlertAction) -> Void)? = nil) {
 //        self.dismissIndicator()
         let alert = UIAlertController(title: title, message: message, preferredStyle: style)
-        let actionDone = UIAlertAction(title: "그런가?", style: .default, handler: handler)
+        let actionDone = UIAlertAction(title: actionDoneButtonTitle, style: .default, handler: handler)
         
         alert.addAction(actionDone)
         
@@ -24,21 +24,21 @@ public extension UIViewController {
     }
     
     // MARK: 커스텀 UIAction이 뜨는 UIAlertController
-//    func presentAlert(title: String,
-//                      message: String? = nil,
-//                      isCancelActionIncluded: Bool = false,
-//                      actionCancelButtonTitle: String = "취소",
-//                      preferredStyle style: UIAlertController.Style = .alert,
-//                      with actions: UIAlertAction ...) {
-////        self.dismissIndicator()
-//        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
-//        actions.forEach { alert.addAction($0) }
-//        if isCancelActionIncluded {
-//            let actionCancel = UIAlertAction(title: actionCancelButtonTitle, style: .cancel, handler: nil)
-//            alert.addAction(actionCancel)
-//        }
-//        self.present(alert, animated: true, completion: nil)
-//    }
+    func presentAlert(title: String,
+                      message: String? = nil,
+                      isCancelActionIncluded: Bool = false,
+                      actionCancelButtonTitle: String = "취소",
+                      preferredStyle style: UIAlertController.Style = .alert,
+                      with actions: UIAlertAction ...) {
+//        self.dismissIndicator()
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        actions.forEach { alert.addAction($0) }
+        if isCancelActionIncluded {
+            let actionCancel = UIAlertAction(title: actionCancelButtonTitle, style: .cancel, handler: nil)
+            alert.addAction(actionCancel)
+        }
+        self.present(alert, animated: true, completion: nil)
+    }
     
     // MARK: 커스텀 하단 경고창
 //    func presentBottomAlert(message: String, target: ConstraintRelatableTarget? = nil, offset: Double? = -12) {
